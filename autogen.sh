ebf8a02b0d34eeb4827706edda806a796aa4c5bd
Philip Chimento <philip.chimento@gmail.com>
2014-05-16 21:33:09 -0700

    Run configure in autogen.sh
    If NOCONFIGURE=1 is not specified, run ./configure after ./autogen.sh.
    This is needed by Jenkins.

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+# NOCONFIGURE is used by gnome-common
+if test -z "$NOCONFIGURE"; then
+    echo "This script will run ./configure automatically. If you wish to pass "
+    echo "any arguments to it, please specify them on the $0 "
+    echo "command line. To disable this behavior, have NOCONFIGURE=1 in your "
+    echo "environment."
+fi
+
 autoreconf -fi
+
+test -n "$NOCONFIGURE" || "./configure" "$@"
